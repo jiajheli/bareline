534faaa519c5c3da50c9d36ccfc0ab65502517db
Jia-Jhe Li <jiajheli@users.noreply.github.com>
2020-10-12 19:53:50 +0800

    Fix: plat_ubuntu

--- plat_ubuntu/bareline_io.h
+++ plat_ubuntu/bareline_io.h
@@ -1,6 +1,9 @@
 #ifndef _BARELINE_IO_H_
 #define _BARELINE_IO_H__ 1
 #include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <printf.h>
 
 #define TERM_XTERM 1
 #define BL_DEBUG 0
@@ -10,8 +13,16 @@ static inline void bl_putc(char c) {
 	return;
 }
 
-static inline char bl_getc(void) {
+static inline uint8_t bl_getc_nb(void) {
 	return (fgetc(stdin) & 0xff);
+}
+
+static inline uint8_t bl_tstc(void) {
+	return 1;
+}
+
+static inline int udelay(int us) {
+	return usleep(us);
 }
 
 #endif
